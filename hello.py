29d54719f34b78f12468721017668007c0bd2939
takahiro-maekawa <koma4024@gmail.com>
2024-12-01 15:15:29 +0900

    APIトークンが読まれていることの確認

--- hello.py
+++ hello.py
@@ -1,8 +1,12 @@
 from notion2md.exporter.block import MarkdownExporter, StringExporter
+from dotenv import load_dotenv
+
+import os
 
 def main():
-    print("Hello from notion2md-test!")
-
-
+    load_dotenv('.env')
+    key = os.environ['NOTION_API_TOKEN']    
+    print("Yout API KEY is {}".format(key))
+    
 if __name__ == "__main__":
     main()
